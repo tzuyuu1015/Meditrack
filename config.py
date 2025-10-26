3f6c7c3d486db4ceca8be765a7e3bd738e29e323
tzuyuu1015 <tzuyuu1015@gmail.com>
2025-10-26 22:01:40 +0800

    Configure database for Render deployment

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
 os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///" + os.path.join(INSTANCE_DIR, "meditrack.db")
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-should-change-this'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'meditrack.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
